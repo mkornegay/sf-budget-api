3230cfd924eaf9823461e9da68786f4e64fe1c27
Michael Kornegay <mjkorneg@outlook.com>
2025-01-27 15:53:31 -0500

    WIP - build out

---
+++ Sf.Budget.Core/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sf.Budget.Core.Repositories
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        void Save(T entity);
+    }
+}
